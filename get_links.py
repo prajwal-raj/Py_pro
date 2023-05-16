70113ab5646ba09b36e2524a3da8f70bd4dad0c5
Prajwal Raj <prajwalraj224@gmail.com>
2023-05-16 23:21:24 +0530

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -5,3 +5,8 @@ url = input("Enter Link: ")
 if ("https" or "http") in url:
     data = rq.get(url)
 else:
+    data = rq.get("https://" + url)
+soup = BeautifulSoup(data.text, "html.parser")
+links = []
+for link in soup.find_all("a"):
+    links.append(link.get("href"))
