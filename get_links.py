9adc60d2070f04c8738d20ee8689a142d5237071
Prajwal Raj <prajwalraj224@gmail.com>
2023-05-16 23:21:53 +0530

    Update get_links.py

--- get_links.py
+++ get_links.py
@@ -10,3 +10,8 @@ soup = BeautifulSoup(data.text, "html.parser")
 links = []
 for link in soup.find_all("a"):
     links.append(link.get("href"))
+
+    # Writing the output to a file (myLinks.txt) instead of to stdout
+# You can change 'a' to 'w' to overwrite the file each time
+with open("myLinks.txt", 'a') as saved:
+    print(links[:10], file=saved)
